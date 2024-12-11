4a7658cb3dab6b00dc04617aa780e23e11a18fc1
Ramonprog <programador.ramon@gmail.com>
2024-12-11 17:59:05 -0300

    feat: finalizando layoute do dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -24,7 +24,7 @@ export default function DashboardLayout({ children }: DashboardLayouteProps) {
           <ModeToggle />
         </div>
       </aside>
-      <main>
+      <main className="p-6 flex flex-col w-full h-full overflow-auto">
         {children}
       </main>
     </div>
