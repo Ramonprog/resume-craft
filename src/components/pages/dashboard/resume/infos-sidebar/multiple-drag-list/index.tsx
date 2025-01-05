43e14b3691cd24b02cee66939806d7e07d7f0c1c
Ramonprog <programador.ramon@gmail.com>
2025-01-05 13:20:47 -0300

    feat: melhorar a lista de arrastar e soltar com chaves dinâmicas e bordas

--- src/components/pages/dashboard/resume/infos-sidebar/multiple-drag-list/index.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/multiple-drag-list/index.tsx
@@ -2,6 +2,7 @@ import { SectionTitle } from "@/components/shared/section-title";
 import { GripVertical, LucideIcon } from "lucide-react";
 import { useFieldArray, useFormContext } from "react-hook-form";
 import { DragDropContext, Draggable, Droppable, DropResult } from '@hello-pangea/dnd'
+import { cn } from "@/lib/utils";
 
 // export type multiple
 
@@ -31,8 +32,11 @@ export function MultipleDragList({ data, onAdd, onEdit }: MultipleDragListProps)
     name: `content.${data.formKey}`
   })
 
-  function handleDrag() {
-    console.log("drag")
+  function handleDrag({ source, destination }: DropResult) {
+    if (!destination) {
+      return;
+    }
+    move(source.index, destination.index);
   }
 
   return (
@@ -46,16 +50,21 @@ export function MultipleDragList({ data, onAdd, onEdit }: MultipleDragListProps)
               {(provided) => (
                 <div {...provided.droppableProps} ref={provided.innerRef} className="rounded overflow-hidden border border-muted">
                   {fields.map((field, index) => {
+                    const titleKey = data.titleKey as keyof typeof field
+                    const descriptionKey = data.descriptionKey as keyof typeof field
+
+                    const isLastItem = index === fields.length - 1;
+
                     return (
                       <Draggable key={`draggable-item-${data.formKey}-${index}`} draggableId={`draggable-item-${data.formKey}-${index}`} index={index}>
                         {(provided) => (
-                          <div key={field.id} ref={provided.innerRef} {...provided.draggableProps} className="h-16 w-full bg-muted/50 flex">
+                          <div key={field.id} ref={provided.innerRef} {...provided.draggableProps} className={cn("h-16 w-full bg-muted/50 flex", !isLastItem && "border-b border-muted/20")}>
                             <div className="bgmuted/50 flex items-center justify-center hover:brightness-105 transition-all">
                               <GripVertical size={14} />
                             </div>
                             <div className="flex-1 flex flex-col justify-center px-3 cursor-pointer hover:bg-muted/80 transition-all">
-                              <p className="text-sm font-title font-bold">Titulo</p>
-                              <p className="text-sx text-muted-foreground">Descrição</p>
+                              <p className="text-sm font-title font-bold">{field[titleKey]}</p>
+                              <p className="text-sx text-muted-foreground">{field[descriptionKey]}</p>
                             </div>
                           </div>
                         )}
