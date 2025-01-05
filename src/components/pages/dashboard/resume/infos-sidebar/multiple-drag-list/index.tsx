8541ccadaf32fa0c3cf603204e8643398c15d4e4
Ramonprog <programador.ramon@gmail.com>
2025-01-05 13:15:57 -0300

    feat: aprimorar lista de arrastar e soltar com ícone e estilo

--- src/components/pages/dashboard/resume/infos-sidebar/multiple-drag-list/index.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/multiple-drag-list/index.tsx
@@ -1,5 +1,5 @@
 import { SectionTitle } from "@/components/shared/section-title";
-import { LucideIcon } from "lucide-react";
+import { GripVertical, LucideIcon } from "lucide-react";
 import { useFieldArray, useFormContext } from "react-hook-form";
 import { DragDropContext, Draggable, Droppable, DropResult } from '@hello-pangea/dnd'
 
@@ -44,19 +44,25 @@ export function MultipleDragList({ data, onAdd, onEdit }: MultipleDragListProps)
           <DragDropContext onDragEnd={handleDrag}>
             <Droppable droppableId={`droppable-${data.formKey}`}>
               {(provided) => (
-                <div {...provided.droppableProps} ref={provided.innerRef} className="bg-red-50">
+                <div {...provided.droppableProps} ref={provided.innerRef} className="rounded overflow-hidden border border-muted">
                   {fields.map((field, index) => {
                     return (
                       <Draggable key={`draggable-item-${data.formKey}-${index}`} draggableId={`draggable-item-${data.formKey}-${index}`} index={index}>
                         {(provided) => (
-                          <div key={field.id} ref={provided.innerRef} {...provided.draggableProps} className="h-16 bg-blue-400">
-                            <div className="w-6 h-full bg-green-300" {...provided.dragHandleProps}></div>
-                            <div className=""><p>content</p></div>
+                          <div key={field.id} ref={provided.innerRef} {...provided.draggableProps} className="h-16 w-full bg-muted/50 flex">
+                            <div className="bgmuted/50 flex items-center justify-center hover:brightness-105 transition-all">
+                              <GripVertical size={14} />
+                            </div>
+                            <div className="flex-1 flex flex-col justify-center px-3 cursor-pointer hover:bg-muted/80 transition-all">
+                              <p className="text-sm font-title font-bold">Titulo</p>
+                              <p className="text-sx text-muted-foreground">Descrição</p>
+                            </div>
                           </div>
                         )}
                       </Draggable>
                     );
                   })}
+                  {provided.placeholder}
                 </div>
               )}
             </Droppable>
